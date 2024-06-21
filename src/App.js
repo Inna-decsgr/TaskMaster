4059ad9f4487a7206ad4b54db84f0055c87d8e7f
Inna-decsgr <anjgkwl27@gmail.com>
2024-06-21 21:25:04 +0900

    Extract Header component into a seperate file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
 import React from 'react';
 import TaskContainer from './components/TaskContainer';
-import { FaCheck } from "react-icons/fa";
+import Header from './components/Header';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 function App() {
-
   return (
-    <div className='p-4'>
-      <h1 className='flex text-3xl font-bold gap-2 mb-4  md:text-6xl md:gap-4'><FaCheck />TaskMaster</h1>
-      <p className='text-sm mb-8 md:text-lg md:mb-16'>TaskMaster를 사용하면 개인 작업을 기록할 수 있습니다.<br />카테고리별로 분리해서 한 눈에 작업들을 편하게 관리해보세요🤗</p>
-      <div className='flex'>
-        <TaskContainer />
-      </div>
-    </div>
+    <DarkModeProvider>
+      <Header />
+      <TaskContainer />
+    </DarkModeProvider>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FaCheck } from "react-icons/fa";
+import { useDarkMode } from '../context/DarkModeContext';
+import { IoBulbOutline } from "react-icons/io5";
+import { FiMoon } from "react-icons/fi";
+
+export default function Header() {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
+  return (
+    <header className='flex justify-between p-4 px-16'>
+      <div className='dark:text-text'>
+        <h1 className='flex text-3xl font-bold gap-2 mb-4 md:text-6xl md:gap-4'><FaCheck />TaskMaster</h1>
+        <p className='text-sm mb-8 md:text-lg md:mb-16'>TaskMaster를 사용하면 개인 작업을 기록할 수 있습니다.<br />카테고리별로 분리해서 한 눈에 작업들을 편하게 관리해보세요🤗</p>
+      </div>
+      <button className='text-4xl dark:text-accent' onClick={toggleDarkMode}>
+        {!darkMode && <IoBulbOutline />}
+        {darkMode && <FiMoon />}
+      </button>
+    </header>
+  );
+}
+
